0de451add98299666048ceda3d14b2642e7dddac
dziugas69 <dziugas.jakeliunas@gmail.com>
2018-04-28 21:48:29 +0300

    Initial commit

---
+++ androidKnyga/app/src/main/java/com/example/dell/androidknyga/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dell.androidknyga;
+
+        import android.app.ListActivity;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+
+    Intent intent;
+    TextView contactId;
+
+    DBTools dbTools = new DBTools(this);
+    protected void onCreate(Bundle savedInstanceState)
+}
+}
